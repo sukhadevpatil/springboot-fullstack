09b95b70d58d2b0787d91fbf6d92ccdc21520d75
Sukhdev Patil <sukhadev.patil@gmail.com>
2023-10-21 19:01:52 +0530

    changes for dynamic data manipulation

--- src/main/java/com/app/crud/fullstack/controller/UserController.java
+++ src/main/java/com/app/crud/fullstack/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.app.crud.fullstack.controller;
 
 import com.app.crud.fullstack.entity.User;
+import com.app.crud.fullstack.exception.UserNotFoundException;
 import com.app.crud.fullstack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @RestController
 @RequestMapping("/api")
 public class UserController {
@@ -22,5 +24,32 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers() {
         return (List<User>) userRepository.findAll();
+    }
+
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUserName(newUser.getUserName());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return  "User with id "+id+" has been deleted success.";
     }
 }
